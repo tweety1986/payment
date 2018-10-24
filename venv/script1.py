8d60d50dfb83e1d543d971458d65383b398ed0b7
tweety1986 <thuy05061986@gmail.com>
2018-10-24 13:25:25 +0200

    fourth

---
+++ venv/script1.py
@@ -0,0 +1,42 @@
+def lista_uslug():
+    lista_uslug = []
+    x = int(input('Podaj, ile usług chcesz dodać?'))
+    for koszta in range(x):
+        x = input("Podaj, nazwę usługi:")
+        lista_uslug.append(x)
+    return lista_uslug
+
+def input_type(lista_uslug):
+    wynik = 0
+    waluta = 'PLN'
+    lista_output = []
+    for wydatki in lista_uslug:
+        x = input('Podaj kwotę za {}:'.format(wydatki))
+        lista_output.append(x)
+    for i in lista_output:
+        wynik = wynik + int(i)
+    print ("Opłaty razem: ", wynik, waluta)
+input_type(lista_uslug())
+
+def lista_przychody():
+    lista_przychody = []
+    x = int(input('Podaj, ile żródeł dochodu posiadasz?'))
+    for pensja in range(x):
+        x = input("Czyj to dochód?")
+        lista_przychody.append(x)
+    return lista_przychody
+
+def input_type(lista_przychody):
+    wynik = 0
+    waluta = 'PLN'
+    lista_output = []
+    for pensja in lista_przychody:
+        x = input('Podaj kwotę przychodu {}:'.format(pensja))
+        lista_output.append(x)
+    for i in lista_output:
+        wynik = wynik + int(i)
+    print ("Przychody razem: ", wynik, waluta)
+input_type(lista_przychody())
+
+
+
