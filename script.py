15bc6a69a6b5f84aa9b0972f52cff2ddfdf53098
tweety1986 <thuy05061986@gmail.com>
2018-10-26 15:17:32 +0200

    for

--- script.py
+++ script.py
@@ -2,11 +2,24 @@ def lista_platnosci():
     lista_platnosci = []
     while True:
         platnosc = input('Płatność za: ')
+        x = input('kwota:')
         lista_platnosci.append(platnosc)
+        lista_platnosci.append(x)
         if platnosc == 'stop':
             break
+
         print(lista_platnosci)
+
 lista_platnosci()
+
+def lista_wynikow(lista_platnosci):
+    lista_wynikow = []
+    for x in range(lista_platnosci):
+        print(int(x))
+lista_wynikow()
+
+
+
 
 
 
